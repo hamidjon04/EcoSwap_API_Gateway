2e5cb53cbc895534d9dbf0d784972a96fe297fa6
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-07-17 09:15:16 +0500

    Add swagger

--- api/handler/center.go
+++ api/handler/center.go
@@ -9,6 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary      Yangi qayta ishlash markazi yaratish
+// @Description  Ushbu endpoint yangi qayta ishlash markazini yaratish uchun ishlatiladi.
+// @Tags         recycling_center
+// @Accept       json
+// @Produce      json
+// @Security 	 ApiKeyAuth
+// @Param        Authorization   header    string       true  "Bearer token"
+// @Param        recycling_center  body      recycling_center.ResCenter  true  "Recycling Center Payload"
+// @Success      200               {object}  recycling_center.ResCenter  "Successful Response"
+// @Failure      400               {object}  model.Error   "Bad Request"
+// @Router       /center/create [post]
 func (h *Handler) CreateRecyclingCenter(c *gin.Context) {
 	req := pb.ResCenter{}
 
@@ -29,6 +40,19 @@ func (h *Handler) CreateRecyclingCenter(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Qayta ishlash markazini qidirish
+// @Description  Ushbu endpoint qayta ishlash markazini qidirish uchun ishlatiladi.
+// @Tags         recycling_center
+// @Accept       json
+// @Produce      json
+// @Security 	 ApiKeyAuth
+// @Param        Authorization   header    string       true  "Bearer token"
+// @Param        limit   query      integer     true        "Limit"
+// @Param        offset  query      integer     true        "Offset"
+// @Param        material query     string      false       "Material type"
+// @Success      200               {object}  recycling_center.ResCenter  "Successful Response"
+// @Failure      400               {object}  model.Error   "Bad Request"
+// @Router       /center/search/{limit}/{offset} [get]
 func (h *Handler) SearchRecyclingCenter(c *gin.Context) {
 	req := pb.FilterField{}
 
@@ -55,6 +79,17 @@ func (h *Handler) SearchRecyclingCenter(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Maxsulot yetkazib berish
+// @Description  Ushbu endpoint maxsulot yetkazib berish uchun ishlatiladi.
+// @Tags         delivery
+// @Accept       json
+// @Produce      json
+// @Security 	 ApiKeyAuth
+// @Param        Authorization   header    string       true  "Bearer token"
+// @Param        delivery_body  body      recycling_center.Submission  true  "Delivery Payload"
+// @Success      200               {object}  recycling_center.Status  "Successful Response"
+// @Failure      400               {object}  model.Error   "Bad Request"
+// @Router       /center/productDelivery [post]
 func (h *Handler) ProductDelivery(c *gin.Context) {
 	req := pb.Submission{}
 
@@ -74,6 +109,18 @@ func (h *Handler) ProductDelivery(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Statistika olish
+// @Description  Ushbu endpoint statistika ma'lumotlarini olish uchun ishlatiladi.
+// @Tags         statistics
+// @Accept       json
+// @Produce      json
+// @Security 	 ApiKeyAuth
+// @Param        Authorization   header    string       true  "Bearer token"
+// @Param        start_date      query     string       true  "Start date in format yyyy-mm-dd"
+// @Param        end_date        query     string       true  "End date in format yyyy-mm-dd"
+// @Success      200             {object}  recycling_center.StatisticField  "Successful Response"
+// @Failure      400             {object}  model.Error       "Bad Request"
+// @Router       /center/statistics     [get]
 func (h *Handler) GetStatistics(c *gin.Context) {
 	req := pb.StatisticField{}
 
--- api/handler/users.go
+++ api/handler/users.go
@@ -9,6 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary      Foydalanuvchi profilini olish
+// @Description  Ushbu endpoint asosida foydalanuvchi profilini olish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string            true  "Bearer token"
+// @Param        id             path     string            true  "Foydalanuvchi identifikatori"
+// @Success      200            {object} users.UserProfile   "Successful response"
+// @Failure      400            {object} model.Error      "Bad request"
+// @Router       /user/getProfile/{id} [get]
 func (h *Handler) GetProfile(c *gin.Context) {
 	resp, err := h.UsersService.GetProfile(c, &pb.UserId{Id: c.Param("id")})
 	if err != nil {
@@ -20,6 +31,17 @@ func (h *Handler) GetProfile(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Foydalanuvchi profilini yangilash
+// @Description  Ushbu endpoint foydalanuvchi profilini yangilash uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string              true  "Bearer token"
+// @Param        body           body     users.ProfileUpdate    true  "Profil yangilash so'rovi"
+// @Success      200            {object} users.Status          "Yangilash muvaffaqiyatli"
+// @Failure      400            {object} model.Error        "Xato so'rov"
+// @Router       /user/updateProfile [put]
 func (h Handler) UpdateProfile(c *gin.Context) {
 	req := pb.ProfileUpdate{}
 
@@ -40,6 +62,17 @@ func (h Handler) UpdateProfile(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Foydalanuvchi profilini o'chirish
+// @Description  Ushbu endpoint foydalanuvchi profilini o'chirish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string       true  "Bearer token"
+// @Param        id             path     string       true  "Foydalanuvchi ID"
+// @Success      200            {object} users.Status   "O'chirish muvaffaqiyatli"
+// @Failure      400            {object} model.Error "Xato so'rov"
+// @Router       /user/deleteProfile/{id} [delete]
 func (h *Handler) DeleteProfile(c *gin.Context) {
 	resp, err := h.UsersService.DeleteProfile(c, &pb.UserId{Id: c.Param("id")})
 	if err != nil {
@@ -51,6 +84,19 @@ func (h *Handler) DeleteProfile(c *gin.Context) {
 	c.JSON(http.StatusOK, resp)
 }
 
+// @Summary      Barcha foydalanuvchilarni olish
+// @Description  Ushbu endpoint barcha foydalanuvchilarni olish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string       true  "Bearer token"
+// @Param        full_name      query    string       false "Foydalanuvchi to'liq ismi"
+// @Param        limit          path     integer      true  "Limit"
+// @Param        offset         path     integer      true  "Offset"
+// @Success      200            {object} users.UsersList "Foydalanuvchilar ro'yxati"
+// @Failure      400            {object} model.Error  "Xato so'rov"
+// @Router       /user/getAllUsers/{limit}/{offset} [get]
 func (h *Handler) GetAllUsers(c *gin.Context) {
 	req := pb.FilterField{}
 
@@ -71,6 +117,90 @@ func (h *Handler) GetAllUsers(c *gin.Context) {
 	resp, err := h.UsersService.GetAllUsers(c, &req)
 	if err != nil {
 		h.Logger.Error(fmt.Sprintf("GetAllUsers request error: %v", err))
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+// @Summary      Foydalanuvchi eko ballarini olish
+// @Description  Ushbu endpoint foydalanuvchi eko ballarini olish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string       true  "Bearer token"
+// @Param        id              path     string       true  "Foydalanuvchi ID"
+// @Success      200            {object} users.UserEcoPoints  "Foydalanuvchi eko ballari"
+// @Failure      400            {object} model.Error       "Xato so'rov"
+// @Router       /user/userEcopoints/{id} [get]
+func(h *Handler) GetEcoPointsByUser(c *gin.Context){
+	resp, err := h.UsersService.GetEcoPointsByUser(c, &pb.UserId{Id: c.Param("id")})
+	if err != nil {
+		h.Logger.Error(fmt.Sprintf("GetEcoPointsByUser request error: %v", err))
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+// @Summary      Foydalanuvchi eko ballarini yaratish
+// @Description  Ushbu endpoint foydalanuvchi uchun eko ballarini yaratish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string            true  "Bearer token"
+// @Param        body           body     users.CreateEcoPoints true  "Foydalanuvchi eko ballari ma'lumotlari"
+// @Success      200            {object} users.Status         "Yaratish muvaffaqiyatli"
+// @Failure      400            {object} model.Error       "Xato so'rov"
+// @Router       /user/createEcopoints [post]
+func(h *Handler) CreateEcoPointsByUser(c *gin.Context){
+	req := pb.CreateEcoPoints{}
+
+	err := c.ShouldBindJSON(&req)
+	if err != nil{
+		h.Logger.Error(fmt.Sprintf("CreateEcoPointsByUser error: %v", err))
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	resp, err := h.UsersService.CreateEcoPointsByUser(c, &req)
+	if err != nil{
+		h.Logger.Error(fmt.Sprintf("CreateEcoPointsByUser request error: %v", err))
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+// @Summary      Foydalanuvchi eko ballari tarixini olish
+// @Description  Ushbu endpoint foydalanuvchi uchun eko ballari tarixini olish uchun ishlatiladi.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Security     ApiKeyAuth
+// @Param        Authorization  header   string            true  "Bearer token"
+// @Param        body           body     users.HistoryReq     true  "Foydalanuvchi eko ballari tarix so'rovi"
+// @Success      200            {object} users.HistoryUserPointsList  "Tarix ro'yxati"
+// @Failure      400            {object} model.Error       "Xato so'rov"
+// @Router       /user/historyUserRcopoints/history [get]
+func(h *Handler) HistoryEcoPointsByUser(c *gin.Context){
+	req := pb.HistoryReq{}
+
+	err := c.ShouldBindJSON(&req)
+	if err != nil{
+		h.Logger.Error(fmt.Sprintf("HistoryEcoPointsByUser error: %v", err))
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	resp, err := h.UsersService.HistoryEcoPointsByUser(c, &req)
+	if err != nil{
+		h.Logger.Error(fmt.Sprintf("HistoryEcoPointsByUser query error: %v", err))
 		c.JSON(http.StatusBadRequest, err)
 		return
 	}
